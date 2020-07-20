161c318d4e71e301d6f0a3d54445c1d4648e9c0e
Alex <alexalpek@gmail.com>
2020-07-20 13:37:06 +0200

    initial commit

---
+++ src/main/java/com/alex/mysickwell/MysickwellApplication.java
@@ -0,0 +1,13 @@
+package com.alex.mysickwell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysickwellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysickwellApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alex/mysickwell/MysickwellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.mysickwell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysickwellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
