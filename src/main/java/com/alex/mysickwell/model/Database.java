ef97ba0061e7514de3c06f7ddbc86cc05c3e7306
Alex <alexalpek@gmail.com>
2020-07-21 22:24:50 +0200

    change in models

--- src/main/java/com/alex/mysickwell/model/Database.java
+++ src/main/java/com/alex/mysickwell/model/Database.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
 
 @Component
@@ -12,4 +13,7 @@ public class Database {
 
     private Map<String, Table> tables = new HashMap<>();
 
+    public void createTable(String tableName, HashMap<Column<?>, LinkedList<Row<?>>> table) {
+        tables.put(tableName, new Table(table));
+    }
 }
--- src/main/java/com/alex/mysickwell/model/Table.java
+++ src/main/java/com/alex/mysickwell/model/Table.java
@@ -1,9 +1,12 @@
 package com.alex.mysickwell.model;
 
+import lombok.AllArgsConstructor;
+
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 
+@AllArgsConstructor
 public class Table {
 
     private Map<Column<?>, LinkedList<Row<?>>> data = new HashMap<>();
