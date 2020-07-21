08b2b85662ebb2cffa0be02761d526d8f33fe51f
Alex <alexalpek@gmail.com>
2020-07-21 22:26:59 +0200

    create table service work in progress day 2

---
+++ src/main/java/com/alex/mysickwell/service/CreateTableService.java
@@ -0,0 +1,50 @@
+package com.alex.mysickwell.service;
+
+import com.alex.mysickwell.model.Column;
+import com.alex.mysickwell.model.Database;
+import com.alex.mysickwell.model.Row;
+import com.alex.mysickwell.util.CreateQueryUtil;
+import com.alex.mysickwell.validation.Middleware;
+import com.alex.mysickwell.validation.create.*;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+@Slf4j
+@Service
+public class CreateTableService {
+
+    private final Logger logger = LoggerFactory.getLogger(CreateTableService.class);
+    private final CreateQueryUtil util;
+    private Database database;
+
+    private Middleware validator;
+
+
+    public ResponseEntity<?> createTable(String query){
+        if (!validator.check(query)) {
+            logger.info("Got malformed query: " + query);
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        String tableName = util.getTableNameFromQuery(query);
+        HashMap<Column<?>, LinkedList<Row<?>>> table = util.getTableColumnsFromQuery(query);
+        database.createTable(tableName, table);
+        logger.info("Table created with name of " + tableName);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @Autowired
+    public CreateTableService(CreateQueryUtil util, Database database, CreateTableValidatorProvider provider) {
+        this.util = util;
+        this.database = database;
+        validator = provider.getMiddleware();
+    }
+}
+
