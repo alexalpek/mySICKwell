245d53d65e64f44f4e382a326aa981d9bdc2b46a
alex <alexalpek@gmail.com>
2020-07-26 16:02:35 +0200

    Test for getTableColumnsFromQuery

--- src/test/java/com/alex/mysickwell/QueryUtils/CreateQueryUtilsTest.java
+++ src/test/java/com/alex/mysickwell/QueryUtils/CreateQueryUtilsTest.java
@@ -1,9 +1,15 @@
 package com.alex.mysickwell.QueryUtils;
 
+import com.alex.mysickwell.model.Column;
+import com.alex.mysickwell.model.ColumnType;
 import com.alex.mysickwell.util.CreateQueryUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -24,6 +30,24 @@ public class CreateQueryUtilsTest {
     @Test
     void getTableNameFromQueryWithSpace(){
         assertEquals("tableNameWithSpace", util.getTableNameFromQuery(queryWithSpace));
+    }
+
+    @Test
+    void getTableColumnsFromQueryTest(){
+        String query = "CREATE TABLE table_name(name VARCHAR, age INTEGER);";
+        Map<Column, LinkedList<?>> result = new LinkedHashMap<>();
+        Column column1 = Column.builder()
+                .name("name")
+                .type(ColumnType.VARCHAR)
+                .build();
+        Column column2 = Column.builder()
+                .name("age")
+                .type(ColumnType.INTEGER)
+                .build();
+
+        result.put(column1, new LinkedList<>());
+        result.put(column2, new LinkedList<>());
+        assertEquals(result.toString(), util.getTableColumnsFromQuery(query).toString());
     }
 
 
