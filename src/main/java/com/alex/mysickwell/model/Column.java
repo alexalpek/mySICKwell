c2f2008213f73c4937ede2d98acc4e2ecd66eff1
Alex <alexalpek@gmail.com>
2020-07-29 07:42:06 +0200

    key immutability

--- src/main/java/com/alex/mysickwell/model/Column.java
+++ src/main/java/com/alex/mysickwell/model/Column.java
@@ -2,16 +2,34 @@ package com.alex.mysickwell.model;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.ToString;
+
+import java.util.Objects;
 
 @Builder
 @ToString
 @Getter
 public class Column {
 
-    private String name;
-    private ColumnType type;
+    @NonNull
+    private final String name;
+    @NonNull
+    private final ColumnType type;
     @Builder.Default
-    private boolean notNull = false;
+    private final boolean notNull = false;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Column column = (Column) o;
+        return name.equals(column.name) &&
+                type == column.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, type);
+    }
 }
